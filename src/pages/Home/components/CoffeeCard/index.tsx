b2cec7117ae66b10ab45ac38b593f279840f8077
unknown <dhoouglas@hotmail.com>
2023-06-21 11:10:30 -0300

    Added data and photos, updated Header/styles, CoffeeCard config

--- src/pages/Home/components/CoffeeCard/index.tsx
+++ src/pages/Home/components/CoffeeCard/index.tsx
@@ -2,28 +2,45 @@ import { ShoppingCart } from "phosphor-react";
 import { QuantityInput } from "../../../../components/QuantityInput";
 import { RegularText, TitleText } from "../../../../components/Typography";
 import { AddCartWrapper, CardFooter, CoffeeCardContainer, Description, Name, Tags } from "./styles";
+import { formatMoney } from "../../../../util/formatMoney";
 
-export function CoffeeCard() {
+export interface Coffee {
+    id: number;
+    tags: string[];
+    name: string;
+    description: string;
+    photo: string;
+    price: number;
+}
+
+interface CoffeeProps {
+    coffee: Coffee;
+}
+
+export function CoffeeCard({ coffee }: CoffeeProps) {
+    const formatPrice = formatMoney(coffee.price);
+
     return (
         <CoffeeCardContainer>
-            <img src="https://s3-alpha-sig.figma.com/img/55b1/f9ee/64600f98b2bae456b96fdc624c4b4f47?Expires=1688342400&Signature=d7q0vnnMDmtDMgOsuOZm0jiWML4zb9YzHeHKh7GQeAtXfLeVaP5DZTzAJy1PwWssAHY~IZ6FtQ6z0IDYHQckTF8koWbkLPykA-1R3gLK7YJVKWxNJ2thEof0tc-oILNO8D5aGC5KVwgy6bgNshM5A-Is1sx0c9hhvehfaWIrRP-SiPwBn51anmPW2WPq9VIbgGhvAZiFxlcO1tdC~hFMohd79WG1KiBPmCwjikfqxa2whaqw6nNEbvdbxNYU9y-ssw8OgYI9yo8Nn2aXd8g1MBULpg9t8kG~Suj4wXDW6fSJNM2QUd6b3ONhtkPA9t2SSxgknsFttLPFD-L7dmrEPw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="" />
+            <img src={`/coffees/${coffee.photo}`} alt="" />
 
             <Tags>
-                <span>Tradicional</span>
-                <span>Com Leite</span>
+                {coffee.tags.map((tag) => (
+                    <span key={`${coffee.id}${tag}`}>{tag}</span>
+                ))}
             </Tags>
 
-            <Name>Expresso Tradicional</Name>
+            <Name>{coffee.name}</Name>
 
             <Description>
-                O tradicional café feito com água quente e grãos moídos
+                {coffee.description}
             </Description>
 
             <CardFooter>
                 <div>
                     <RegularText size="s">R$</RegularText>
                     <TitleText size="m" color="text" as="strong">
-                        90,00    
+                        {formatPrice}   
                     </TitleText>    
                 </div> 
 
