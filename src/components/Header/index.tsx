0f0973c4ede321fbbdc7a535bca0266ae93c7e72
unknown <dhoouglas@hotmail.com>
2023-06-22 09:46:39 -0300

    Edited Header navigation, and created CompleteOrderForm and SectionTitle components of the CompleteOrder page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,26 @@
 import { HeaderButton, HeaderButtonContainer, HeaderContainer } from "./styles";
 import coffeeLogoImg from "../../assets/coffee-delivery-logo.svg";
 import { MapPin, ShoppingCart } from "phosphor-react";
+import { NavLink } from "react-router-dom";
 
 export function Header() {
     return (
         <HeaderContainer>
             <div className="container">
-                <img src={coffeeLogoImg} alt="" />
+                <NavLink to="/">
+                    <img src={coffeeLogoImg} alt="" />
+                </NavLink>
                 <HeaderButtonContainer>
                     <HeaderButton variant="purple">
                         <MapPin size={20} weight="fill" />
                         Belford Roxo, RJ
                     </HeaderButton>
 
-                    <HeaderButton variant="yellow">
-                        <ShoppingCart size={20} weight="fill" />
-                    </HeaderButton>
+                    <NavLink to="/completeOrder">
+                        <HeaderButton variant="yellow">
+                            <ShoppingCart size={20} weight="fill" />
+                        </HeaderButton>
+                    </NavLink>
                 </HeaderButtonContainer>
             </div>
         </HeaderContainer>
